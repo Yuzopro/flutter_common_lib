a7a97fa2429c16e98c0931d44d2171f6b24c9870
yuzo <yuzopro@gmail.com>
2021-04-16 16:07:15 +0800

    修改升级flutter到2.1.0之后不能编译的问题

--- flutter_base_ui/lib/bloc/bloc_provider.dart
+++ flutter_base_ui/lib/bloc/bloc_provider.dart
@@ -17,8 +17,8 @@ class BlocProvider<T extends BaseBloc> extends StatefulWidget {
   }
 
   static T of<T extends BaseBloc>(BuildContext context) {
-    final type = _typeOf<BlocProvider<T>>();
-    BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    BlocProvider<T> provider =
+    context.findAncestorWidgetOfExactType<BlocProvider<T>>();
     return provider.bloc;
   }
 
